1c684acbd1cbde12454563f307537305d6e1bb37
Jennifer Stewart <jstewart8053@gmail.com>
2020-09-18 12:20:56 -0500

    MVP... working on stretch

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosWithAuth = () => {
-    // create an instance of axios with a global config setup
+
     const token = localStorage.getItem("token");
     return axios.create({
         headers: {
